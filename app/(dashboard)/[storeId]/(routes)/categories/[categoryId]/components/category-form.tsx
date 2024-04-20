b691e81c7a505954dd68c732bc2e36461dcc7f83
Drkrharsh <drkrharsh@gmail.com>
2024-04-19 20:08:16 -0700

    update

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Billboard, Store } from "@prisma/client"
+import { Category} from "@prisma/client"
 import axios from "axios";
 import { Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
@@ -20,17 +20,17 @@ import * as z  from "zod";
 
 
 const formSchema = z.object({
-    label: z.string().min(1),
-    imageUrl: z.string().min(1),
+    name: z.string().min(1),
+    billboardId: z.string().min(1),
 
 });
 
-type BillboardFormValues = z.infer<typeof formSchema>;
+type CategoryFormValues = z.infer<typeof formSchema>;
 
-interface BillboardFormProps {
-    initialData: Billboard | null;
+interface CategoryFormProps {
+    initialData: Category | null;
 }
-export const BillboardForm: React.FC<BillboardFormProps> = ({
+export const CategoryForm: React.FC<CategoryFormProps> = ({
     initialData
 }) => {
     const params = useParams();
@@ -39,19 +39,19 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const title = initialData ? "Edit billboard" : "Create billboard";
-    const description = initialData ? "Edit a billboard" : "Add a new billboard";
-    const toastMessage = initialData ? "Billboard updated." : "Billboard created.";
+    const title = initialData ? "Edit category" : "Create category";
+    const description = initialData ? "Edit a category" : "Add a new category";
+    const toastMessage = initialData ? "Category updated." : "Category created.";
     const action = initialData ? "Save changes" : "Create";
     
-    const form = useForm<BillboardFormValues>({
+    const form = useForm<CategoryFormValues>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
-            label: '',
-            imageUrl:''
+            name: '',
+            billboardId:''
         }
     });
-    const onSubmit = async (data: BillboardFormValues) => {
+    const onSubmit = async (data: CategoryFormValues) => {
         try {
             setLoading(true);
             if(initialData) {
@@ -111,33 +111,15 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         <Separator />
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-                <FormField
-                    control={form.control}
-                    name= "imageUrl"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Background image</FormLabel>
-                            <FormControl>
-                                <ImageUpload 
-                                    value={field.value ? [field.value] : []}
-                                    disabled={loading}
-                                    onChange={(url) => field.onChange(url)}
-                                    onRemove={() => field.onChange("")}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
                 <div className="grid grid-cols-3 gap-8">
                     <FormField
                     control={form.control}
-                    name= "label"
+                    name= "name"
                     render={({field}) => (
                         <FormItem>
-                            <FormLabel>Label</FormLabel>
+                            <FormLabel>Name</FormLabel>
                             <FormControl>
-                                <Input disabled={loading} placeholder="Billboard label" {...field}/>
+                                <Input disabled={loading} placeholder="Category name" {...field}/>
                             </FormControl>
                             <FormMessage />
                         </FormItem>
